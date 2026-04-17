37244ad9871ebecdcfa86b36278292053e1b7266
thanhyt123 <23010764@st.phenikaa-uni.edu.vn>
2026-04-17 07:54:17 +0700

    Create NumberClassifierTest.java

---
+++ NumberClassifierTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class NumberClassifierTest {
+
+    @Test
+    public void testMoreEvens() {
+        int[] arr = {2, 4, 6};
+        assertEquals("More evens", NumberClassifier.classifyNumbers(arr));
+    }
+
+    @Test
+    public void testMoreOdds() {
+        int[] arr = {1, 3, 5};
+        assertEquals("More odds", NumberClassifier.classifyNumbers(arr));
+    }
+
+    @Test
+    public void testEqual() {
+        int[] arr = {1, 2};
+        assertEquals("Equal", NumberClassifier.classifyNumbers(arr));
+    }
+}
